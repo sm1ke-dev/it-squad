86f20ec9e983a2aa073b22092a9bd058c190f210
sm1ke <smike143@yandex.ru>
2022-12-19 00:55:54 +0500

    fix: исправить внутренние ссылки

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const burgerButton = document.querySelector('.header__burger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeButton = mobileMenu.querySelector('.mobile-menu__button');
+const mobileMenuLinks = [...mobileMenu.querySelectorAll('.mobile-menu__link')];
 
 const openMenu = () => {
   mobileMenu.classList.add('mobile-menu_opened');
@@ -9,6 +10,10 @@ const openMenu = () => {
 const closeMenu = () => {
   mobileMenu.classList.remove('mobile-menu_opened');
 }
+
+mobileMenuLinks.forEach(link => {
+  link.addEventListener('click', closeMenu);
+})
 
 burgerButton.addEventListener('click', openMenu);
 closeButton.addEventListener('click', closeMenu);
