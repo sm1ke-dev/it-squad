f1fe1504ba453c2103e6f9b9818992267d6b02a4
sm1ke <smike143@yandex.ru>
2022-12-19 18:39:26 +0500

    feat: сделать hover на ссылки

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,29 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const closeButton = mobileMenu.querySelector('.mobile-menu__button');
 const mobileMenuLinks = [...mobileMenu.querySelectorAll('.mobile-menu__link')];
 
+const listItem = [...document.querySelectorAll('.customers__item')];
+
+listItem.forEach(item => {
+  const link = item.querySelector('.customers__text');
+  const arrow = item.querySelector('.button');
+  const pic = item.querySelector(`#${link.id}-pic`);
+
+  link.addEventListener('mouseover', () => {
+    arrow.classList.add('button_hover');
+
+    if (pic) {
+      pic.classList.add(`customers__photo-${link.id}`);
+    } else {
+      console.log('Пасхалочка');
+    }
+  });
+
+  link.addEventListener('mouseout', () => {
+    arrow.classList.remove('button_hover');
+    pic.classList.remove(`customers__photo-${link.id}`);
+  });
+});
+
 const openMenu = () => {
   mobileMenu.classList.add('mobile-menu_opened');
 }
